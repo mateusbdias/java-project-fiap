3d34e544fbe25b7d15627e78d6e4156c91923bd7
Mateus Benites Dias <mateusdias@outlook.com>
2022-09-05 10:42:40 -0300

    2022-09-05 - FIAP Nano Course Java - Aula 05 - Coleções

--- src/br/com/fiap/java/EstruturasRepeticao_Arrays_Strings_Colecoes/Colecoes.java
+++ src/br/com/fiap/java/EstruturasRepeticao_Arrays_Strings_Colecoes/Colecoes.java
@@ -1,12 +1,15 @@
 package br.com.fiap.java.EstruturasRepeticao_Arrays_Strings_Colecoes;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 
 public class Colecoes {
 	
 	public static void main(String[] args) {
 		
+		// ArrayList
 		List<String> lista = new ArrayList<>();
 		lista.add("LTP");
 		lista.add("Web");
@@ -18,6 +21,25 @@ public class Colecoes {
 
 		int indice = lista.indexOf("Web");
 		System.out.println("O valor \"Web\" está na posição " + indice);
+		
+		// HashSet
+		HashSet<String> cursos = new HashSet<>();
+		cursos.add("Java");
+		cursos.add(".NET");
+		cursos.add("Android");
+		cursos.add("Java");
+		
+		System.out.println(cursos);
+		
+		// HashMap
+		HashMap<String, String> mapa = new HashMap<>();
+		mapa.put("RM1234", "Thiago");
+		mapa.put("RM4321", "João");
+		
+		System.out.println(mapa);
+		System.out.println(mapa.get("RM1234"));
+		mapa.remove("RM1234");
+		System.out.println(mapa.get("RM1234"));
 		
 	}
 
